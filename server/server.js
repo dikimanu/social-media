a2623d0f541d5ef4a2c735044297270139ae7a36
only-recent <only007895@gmail.com>
2026-01-09 11:14:33 +0530

    Describe your change

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import {inngest, functions} from './ingest/index.js'
+import {inngest, functions} from './inngest/index.js'
+import {serve} from 'inngest/express'
 
 const app = express();
 
